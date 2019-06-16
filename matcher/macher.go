a76b09cda079948906e6f32ed75765ff6da40b3e
nqdinh <nqdinhddt@gmail.com>
2019-06-16 22:24:32 +0700

    trie test

--- matcher/macher.go
+++ matcher/macher.go
@@ -10,13 +10,20 @@ const (
 // Handler is a value associated with a subscription.
 type Handler interface{}
 
+// Subscription represents a topic subscription.
+type Subscription struct {
+	id      uint32
+	topic   string
+	handler Handler
+}
+
 // Matcher contains topic subscriptions and performs matches on them.
 type Matcher interface {
 	// Subscribe adds the Subscriber to the topic and returns a Subscription.
-	Add(topic string, hdl Handler) error
+	Add(topic string, hdl Handler) (*Subscription, error)
 
 	// Unsubscribe removes the Subscription.
-	Remove(topic string, hdl Handler) error
+	Remove(*Subscription) error
 
 	// Lookup returns the Subscribers for the given topic.
 	Lookup(topic string) []Handler
--- matcher/trie_test.go
+++ matcher/trie_test.go
@@ -9,43 +9,50 @@ import (
 func TestMatcher(t *testing.T) {
 	assert := assert.New(t)
 	var (
-		m  = NewMatcher()
+		m  = NewTrieMatcher()
 		s0 = 0
 		s1 = 1
 		s2 = 2
 	)
-	sub0, err := m.Subscribe("forex.*", s0)
+	sub0, err := m.Add("forex.*", s0)
 	assert.NoError(err)
-	sub1, err := m.Subscribe("*.usd", s0)
+	sub1, err := m.Add("*.usd", s0)
 	assert.NoError(err)
-	sub2, err := m.Subscribe("forex.eur", s0)
+	sub2, err := m.Add("forex.eur", s0)
 	assert.NoError(err)
-	sub3, err := m.Subscribe("*.eur", s1)
+	sub3, err := m.Add("*.eur", s1)
 	assert.NoError(err)
-	sub4, err := m.Subscribe("forex.*", s1)
+	sub4, err := m.Add("forex.*", s1)
 	assert.NoError(err)
-	sub5, err := m.Subscribe("trade", s1)
+	sub5, err := m.Add("trade", s1)
 	assert.NoError(err)
-	sub6, err := m.Subscribe("*", s2)
+	sub6, err := m.Add("*", s2)
 	assert.NoError(err)
 
-	assertEqual(assert, []Subscriber{s0, s1}, m.Lookup("forex.eur"))
-	assertEqual(assert, []Subscriber{s2}, m.Lookup("forex"))
-	assertEqual(assert, []Subscriber{}, m.Lookup("trade.jpy"))
-	assertEqual(assert, []Subscriber{s0, s1}, m.Lookup("forex.jpy"))
-	assertEqual(assert, []Subscriber{s1, s2}, m.Lookup("trade"))
+	assertEqual(assert, []Handler{s0, s1}, m.Lookup("forex.eur"))
+	assertEqual(assert, []Handler{s2}, m.Lookup("forex"))
+	assertEqual(assert, []Handler{}, m.Lookup("trade.jpy"))
+	assertEqual(assert, []Handler{s0, s1}, m.Lookup("forex.jpy"))
+	assertEqual(assert, []Handler{s1, s2}, m.Lookup("trade"))
 
-	m.Unsubscribe(sub0)
-	m.Unsubscribe(sub1)
-	m.Unsubscribe(sub2)
-	m.Unsubscribe(sub3)
-	m.Unsubscribe(sub4)
-	m.Unsubscribe(sub5)
-	m.Unsubscribe(sub6)
+	m.Remove(sub0)
+	m.Remove(sub1)
+	m.Remove(sub2)
+	m.Remove(sub3)
+	m.Remove(sub4)
+	m.Remove(sub5)
+	m.Remove(sub6)
 
-	assertEqual(assert, []Subscriber{}, m.Lookup("forex.eur"))
-	assertEqual(assert, []Subscriber{}, m.Lookup("forex"))
-	assertEqual(assert, []Subscriber{}, m.Lookup("trade.jpy"))
-	assertEqual(assert, []Subscriber{}, m.Lookup("forex.jpy"))
-	assertEqual(assert, []Subscriber{}, m.Lookup("trade"))
+	assertEqual(assert, []Handler{}, m.Lookup("forex.eur"))
+	assertEqual(assert, []Handler{}, m.Lookup("forex"))
+	assertEqual(assert, []Handler{}, m.Lookup("trade.jpy"))
+	assertEqual(assert, []Handler{}, m.Lookup("forex.jpy"))
+	assertEqual(assert, []Handler{}, m.Lookup("trade"))
+}
+
+func assertEqual(assert *assert.Assertions, expected, actual []Handler) {
+	assert.Len(actual, len(expected))
+	for _, sub := range expected {
+		assert.Contains(actual, sub)
+	}
 }
